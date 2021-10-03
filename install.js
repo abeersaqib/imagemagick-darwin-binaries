5acc1723a41c4630e0becbcc1d4766678e54c79b
Muhammad Abeer <abeersaqib948@gmail.com>
2021-10-03 22:41:38 +0500

    Update install.js

--- install.js
+++ install.js
@@ -5,11 +5,11 @@ var fse = require('fs-extra');
 var os = require('os');
 var path = require('path');
 var targz = require('node-tar.gz');
-var version = "7.0.7";
+var version = "ImageMagick-x86_64-apple-darwin20.1.0";
 
 // Using callbacks
 console.log('Going to extract compressed imagemagick library, version: '+version);
-targz().extract(path.join(__dirname, '/bundle/' + version + '.tar.gz'), path.join(__dirname, 'bin/osx/imagemagick'), function (err) {
+targz().extract(path.join(__dirname, '/' + version + '.tar.gz'), path.join(__dirname, 'bin/osx/imagemagick'), function (err) {
   if (err)
     console.log('Something is wrong ', err.stack);
 
